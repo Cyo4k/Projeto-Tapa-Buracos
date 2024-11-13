39a6a85bea451ce857b216f93938a951bcb1ebdc
João <jpauloxpo@gmail.com>
2024-11-13 14:19:01 -0300

    Configurações gerais

--- serve.js
+++ serve.js
@@ -1,25 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const conect = "mongodb+srv://jpauloxpo:1234567Iso@tapaburacos.zwl1r.mongodb.net/?retryWrites=true&w=majority&appName=tapaBuracos"
-//const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const conect =
+    `mongodb+srv://${DB_USER}:${DB_PASS}@tapaburacos.zwl1r.mongodb.net/?retryWrites=true&w=majority&appName=tapaBuracos`;
+const cors = require("cors");
 
-const authRoutes = require('./routes/authRoutes');
-const reportRoutes = require('./routes/reportRoutes');
+const authRoutes = require("./routes/authRoutes");
+const reportRoutes = require("./routes/reportRoutes");
 
 const app = express();
-/*app.use(cors());
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));*/
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(conect, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('banco de dados conectado com sucesso!'))
-  .catch((err) => console.log(err));
+mongoose
+    .connect(conect, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("banco de dados conectado com sucesso!"))
+    .catch((err) => console.log(err));
 
-app.use('/auth', authRoutes);
-app.use('/reports', reportRoutes);
+app.use("/auth", authRoutes);
+app.use("/reports", reportRoutes);
 
 app.listen(5000, () => {
-  console.log('Server running on port 5000');
+    console.log("Server running on port 5000");
 });
